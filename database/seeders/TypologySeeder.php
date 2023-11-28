dc9b41a0535285d9d6f683c25946b4a67e2e8643
Marco-Vigilio <tonyvigilio@gmail.com>
2023-11-28 12:36:20 +0100

    seed TypologySeeder

--- database/seeders/TypologySeeder.php
+++ database/seeders/TypologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Admin\Typology;
 
 class TypologySeeder extends Seeder
 {
@@ -12,8 +14,21 @@ class TypologySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $tipologie = [
+            ["name" => "Ristorante"],
+            ["name" => "Pizzeria"],
+            ["name" => "Sushi"],
+            ["name" => "Fast Food"],
+            ["name" => "Poke"],
+        ];
+
+
+        foreach ($tipologie as $tipologia) {
+            $newTypology = new Typology();
+            $newTypology->name = $tipologia["name"];
+            $newTypology->save();
+        }
     }
 }
