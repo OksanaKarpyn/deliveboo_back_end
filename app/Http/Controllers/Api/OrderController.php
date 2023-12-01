056383aaab2a8b603227e794a856a16b914bf5e5
Oksana Karpyn <oksana101992@gmail.com>
2023-12-01 10:57:00 +0100

    api contoller

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin\Restaurant;
+
+class RestaurantController extends Controller
+{
+    public function index() {
+
+
+        $restaurants = Restaurant::with('typologies','dishes')->get();
+
+        $response = [
+            "success" => true,
+            "results" => $restaurants
+        ];
+
+        return response()->json($response);
+
+    }
+
+}
