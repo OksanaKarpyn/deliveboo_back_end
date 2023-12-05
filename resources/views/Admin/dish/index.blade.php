61ea55de1ef6cf39ad214f4dd576d2d5464a9141
salvatoredepaola <salvatoredepaola8@gmail.com>
2023-12-05 11:23:57 +0100

    dishes index style

--- resources/views/Admin/dish/index.blade.php
+++ resources/views/Admin/dish/index.blade.php
@@ -1,34 +1,48 @@
 @extends('layouts.app')
 @section('content')
-   <h1>i tuoi piatti</h1>
-   <a href="/dishes/create">Aggiungi piatto</a>
-   @if(isset($dishes))
-    @foreach ($dishes as $dish)
-        <h2>{{$dish->name}}</h2>
-        <h2>{{$dish->ingredients}}</h2>
-        <p>{{$dish->price}}</p>
-        <a href="{{ route('dishes.edit', $dish->id) }}" class="btn btn-primary btn-sm">Modifica</a>
-          <form action="{{ route('dishes.destroy', $dish->id) }}"     method="post">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger btn-sm">Elimina</button>
-           </form>
+<div class="container py-4 p-2">
+    <div>
+        <h1 class="text-dark me-3 ">Menu</h1>
+        <a class="btn btn-success text-white me-3 fs-5 my-5" href="{{ route('dashboard.index') }}">&larr; Home Page</a>
+        <a class="btn btn-success text-white me-3 fs-5 my-5" href="{{ route('dishes.create') }}">Crea Nuovo Piatto</a>
+        <div class="container">
+            <div class="row">
+                <table>
+                    <thead>
+                        <tr>
+                          <th scope="col" >Id</th>
+                          <th scope="col">Nome Piatto</th>
+                          <th scope="col">Disponibilità</th>
+                          <th scope="col">Prezzo</th>
+                          <th scope="col">Azioni</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach ($dishes as $index => $dish )
+                            <tr>
+                                <th scope="row">{{ $index + 1}}</th>
+                                <td><a class="text-decoration-none text-black text-capitalize" href="{{ route('dishes.show', $dish) }}">{{ $dish->name }}</a></td>
+                                <td>{{ ($dish->visible === 1) ? "Available" : "Not Available" }}</td>
+                                <td>{{ $dish->price }} &euro;</td> 
+                                <td>
+                                    <a class="btn btn-success text-white me-4 " href="{{ route('dishes.show', $dish) }}">Show</a>
 
+                                    <a class="btn btn-warning me-4 m-1" href="{{ route('dishes.edit', $dish) }}">Edit</a>
+                                    <form action="{{ route('dishes.destroy', $dish->id) }}" class="d-inline-block "    method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger ">Elimina</button>
+                                    </form>
+                                </td>
+                            </tr>
 
-           <div class="p-3 col-sm-12 col-md-12 col-xl-12 col-xxl-3 bordo-r">
-                @if($dish->photo)
-                    <img class="rounded-circle col-6"  src="{{ asset('storage/' . $dish->photo) }}" alt="foto">    
-                @else
-                   <div class="img-link d-flex justify-content-center align-items-center">
-                       <img class="doctor-img rounded-circle" src="https://picsum.photos/200/300" alt="">    
-                   </div>
-
-                @endif
-           </div>
-        <a href="{{ route('dishes.show', $dish->id) }}" class="btn btn-info">show</a>
-    @endforeach
-    
-    @endif
+                        @endforeach
+                      </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
   
     
 @endsection
